af527e6a9e14b8eb996b95f3dd8b2438fad34fa2
Nel-sokchhunly <nel.sokchhunly19@kit.edu.kh>
2023-05-17 09:27:43 +0700

    chore: add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,23 @@
 import { createApp } from 'vue';
 import { createWebHistory, createRouter } from 'vue-router';
+import { createPinia } from 'pinia';
 
 import './style.css';
 import App from './App.vue';
 
+// router
 import routes from './router';
-
 const router = createRouter({
   // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHistory(),
   routes // short for `routes: routes`
 });
 
+// state management
+const pinia = createPinia();
+
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 app.mount('#app');
